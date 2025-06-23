bce7122ff0f8cde740f8511bc28db695fe134597
Harsh <hs5686584@gmail.com>
2025-06-23 10:06:44 +0530

    navigation setup done

--- App.jsx
+++ App.jsx
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 const App = () => {
+  const Stack = createStackNavigator();
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={LoginScreen} name="login" />
+        <Stack.Screen component={SignupScreen} name="signup" />
+        <Stack.Screen component={WelcomeScreen} name="welcome" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
+  const navigation = useNavigation();
+
+  const redirection = () => {
+    navigation.navigate('welcome');
+  };
   return (
     <View>
       <Text>LoginScreen</Text>
+      <TouchableOpacity onPress={redirection}>
+        <Text>Navigate</Text>
+      </TouchableOpacity>
     </View>
   );
 };
